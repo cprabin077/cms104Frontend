9c522ef1e20ab1a7c997759d0a401d6dbe0cfd28
= <prabin.chaudhary3107@gmail.com>
2026-02-02 10:22:32 +0545

    Day 15 implement frontend and backend using cors

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -1,29 +1,28 @@
-
 import React from 'react'
 import './Navbar.css'
 
 const Navbar = () => {
   return (
     <div>
-            <nav class="navbar">
-                <div class="nav-container">
-                    <div class="logo">Mero<span>Blog</span></div>
+            <nav className="navbar">
+                <div className="nav-container">
+                    <div className="logo">Mero<span>Blog</span></div>
 
-                    <div class="nav-links">
-                        <a href="/" class="active">Home</a>
+                    <div className="nav-links">
+                        <a href="/">Home</a>
                         <a href="/singleBlog">Single Blog</a>
                         <a href="/createBlog">Create Blog</a>
                         <a href="/updateBlog">Update Blog</a>
                     </div>
 
-                    <div class="nav-actions">
-                        <a href="/signin" class="cta-button">Signin</a>
+                    <div className="nav-actions">
+                        <a href="/signin" className="cta-button">Signin</a>
                     </div>
                 </div>
             </nav>
 
-            <section class="hero">
-                <div class="hero-content">
+            <section className="hero">
+                <div className="hero-content">
                     <h1>Organize Your Thoughts</h1>
                     <p>A clean space for your blogs and todos.</p>
                 </div>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,18 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../../component/Navbar/Navbar'
+import axios from 'axios'
 
 const Home = () => {
+    const [blogs, setBlogs] = useState([])
+
+    // api call garne
+
+    const fetchBlogs = async () => {
+        const response = await axios.get('http://localhost:3000/blogs')
+
+        setBlogs(response.data.blogs)
+       // console.log(response.data.blogs)
+    }
+
+    useEffect(() => {
+        fetchBlogs()
+    }, [])
+
+    //console.log(blogs, "Blogs")
+
     return (
         <div>
             <Navbar />
-            <div className="card" style={{width:  '18rem'}}>
-                    <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <h5 className="card-title">Card subTitle</h5>
-                        
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" className="btn btn-primary">Go somewhere</a>
-                    </div>
+            <div className="card" style={{ width: '18rem' }}>
+                {blogs.map((blog) =>{
+                        return(
+                <div key={blog._id}className="card-body">
+                    <h5 className="card-title">{blog.title}</h5>
+                    <h5 className="card-title">{blog.subTitle}</h5>
+
+                    <p className="card-text">{blog.description}</p>
+                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                </div>
+                )
+                    })}
             </div>
         </div>
     )
