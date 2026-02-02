ded54e8dba83844d17bc60936dfbb4d6872c8996
= <prabin.chaudhary3107@gmail.com>
2026-02-02 22:31:00 +0545

    day 16 loops(foreach, map)

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,20 +3,28 @@ import Navbar from '../../component/Navbar/Navbar'
 import axios from 'axios'
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([])
+    const [blogs, setBlogs] = useState([]);
 
     // api call garne
 
     const fetchBlogs = async () => {
-        const response = await axios.get('http://localhost:3000/blogs')
-
-        setBlogs(response.data.blogs)
-       // console.log(response.data.blogs)
+        try {
+            const response = await axios.get('http://localhost:2000/blogs')
+            if(response.status==200){
+                setBlogs(response.data.blogs)
+            }else{
+                alert("Something wwent wrong")
+            }
+            
+        } catch (error) {
+            alert("Something went wrong")
+        }
+        // console.log(response.data.blogs)
     }
 
     useEffect(() => {
         fetchBlogs()
-    }, [])
+    }, []);
 
     //console.log(blogs, "Blogs")
 
@@ -24,17 +32,17 @@ const Home = () => {
         <div>
             <Navbar />
             <div className="card" style={{ width: '18rem' }}>
-                {blogs.map((blog) =>{
-                        return(
-                <div key={blog._id}className="card-body">
-                    <h5 className="card-title">{blog.title}</h5>
-                    <h5 className="card-title">{blog.subTitle}</h5>
+                {blogs.map((blog) => {
+                    return (
+                        <div key={blog._id} className="card-body">
+                            <h5 className="card-title">{blog.title}</h5>
+                            <h5 className="card-title">{blog.subTitle}</h5>
 
-                    <p className="card-text">{blog.description}</p>
-                    <a href="#" className="btn btn-primary">Go somewhere</a>
-                </div>
-                )
-                    })}
+                            <p className="card-text">{blog.description}</p>
+                            <a href="#" className="btn btn-primary">Go somewhere</a>
+                        </div>
+                    )
+                })}
             </div>
         </div>
     )
