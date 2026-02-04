45a42407baa4e12775cbd7dc600c07b13d80c617
= <prabin.chaudhary3107@gmail.com>
2026-02-04 17:21:31 +0545

    day 17 third approach

--- src/pages/CreateBlog/CreateBlog.jsx
+++ src/pages/CreateBlog/CreateBlog.jsx
@@ -7,23 +7,35 @@ import { data, useNavigate } from 'react-router-dom'
 const CreateBlog = () => {
 
   const navigate = useNavigate()
+  const [data, setData] = useState({
+    title: "",
+    subTitle: "",
+    description: ""
+  })
 
-  //Second approach
-  const createBlog = async (e) =>{
-      e.preventDefault()
-      console.log(e.currentTarget,"currentTarget")
-      const formData = new FormData(e.currentTarget)
-     
-      return
-    // send above status data to api
-      const response = await axios.post("http://localhost:2000/blogs")
-      if(response.status == 201){
-        alert("resposne.data.message")
-        navigate("/")
-      }else{
-        alert("Something went wrong")
-      }
+  //Third approach
+  const createBlog = async (e) => {
+    e.preventDefault()
+    // send above states data to API
+    const response = await axios.post("http://localhost:2000/blogs", data)
+    if (response.status == 201) {
+      alert(response.data.message)
+      navigate("")
+    } else {
+      alert("Something went wrong")
+    }
+
   }
+
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    setData({
+      ...data,
+      [name]: value
+    })
+  }
+
+
 
   return (
     <div>
@@ -34,17 +46,17 @@ const CreateBlog = () => {
         <form onSubmit={createBlog}>
           <div className="form-group">
             <label htmlFor="title">Title</label>
-            <input type="text" id="title" name = "title" placeholder="Enter title" />
+            <input type="text" id="title" name="title" placeholder="Enter title" onChange={handleChange} />
           </div>
 
           <div className="form-group">
             <label htmlFor="subtitle">Subtitle</label>
-            <input type="text" id="subTitle" name="subTitle" placeholder="Enter subtitle"  />
+            <input type="text" id="subTitle" name="subTitle" placeholder="Enter subtitle" onChange={handleChange} />
           </div>
 
           <div className="form-group">
             <label htmlFor="description">Description</label>
-            <textarea id="description" name="description" placeholder="Enter description"
+            <textarea id="description" name="description" placeholder="Enter description" onChange={handleChange}
             ></textarea>
           </div>
 
